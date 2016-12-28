d8cabb5b04d2b2c4adfe889ea691ddf1aa96bd8f
Ben Nuttall <ben@bennuttall.com>
2016-12-28 01:05:10 +0000

    Refactor to use itertools.product over loops

--- uno.py
+++ uno.py
@@ -1,4 +1,5 @@
-import random
+from random import shuffle
+from itertools import product, repeat, chain
 
 
 COLORS = ['red', 'yellow', 'green', 'blue']
@@ -12,27 +13,22 @@ CARD_TYPES = NUMBERS + SPECIAL_CARD_TYPES + BLACK_CARD_TYPES
 
 class UnoCard:
     def __init__(self, color, card_type):
+        self._validate(color, card_type)
+        self.color = color
+        self.card_type = card_type
+
+    def _validate(self, color, card_type):
         if color not in ALL_COLORS:
             raise ValueError('Invalid color')
-        self.color = color
-
-        if card_type not in CARD_TYPES:
-            raise ValueError('Invalid card type')
         if color == 'black' and card_type not in BLACK_CARD_TYPES:
             raise ValueError('Invalid card type')
-        if color != 'black' and card_type in BLACK_CARD_TYPES:
+        if color != 'black' and card_type not in COLOR_CARD_TYPES:
             raise ValueError('Invalid card type')
-        self.card_type = card_type
 
 
 def create_uno_deck():
-    deck = []
-    for color in COLORS:
-        for card_type in COLOR_CARD_TYPES:
-            deck.append(UnoCard(color, card_type))
-    for card_type in BLACK_CARD_TYPES:
-        for i in range(4):
-            deck.append(UnoCard('black', card_type))
-
-    random.shuffle(deck)
+    color_cards = product(COLORS, COLOR_CARD_TYPES)
+    black_cards = product(repeat('black', 4), BLACK_CARD_TYPES)
+    deck = list(chain(color_cards, black_cards))
+    shuffle(deck)
     return deck
