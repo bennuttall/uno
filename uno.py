21bc279e862e5f9fa10618396d7ef51474d0137a
Ben Nuttall <ben@bennuttall.com>
2016-12-30 12:03:27 +0000

    Begin to implement Uno Game

--- uno.py
+++ uno.py
@@ -16,6 +16,7 @@ class UnoCard:
         self._validate(color, card_type)
         self.color = color
         self.card_type = card_type
+        self.temp_color = None
 
     def _validate(self, color, card_type):
         if color not in ALL_COLORS:
@@ -25,10 +26,53 @@ class UnoCard:
         if color != 'black' and card_type not in COLOR_CARD_TYPES:
             raise ValueError('Invalid card type')
 
+    @property
+    def _color(self):
+        return self.temp_color if self.temp_color else self.color
 
-def create_uno_deck():
-    color_cards = product(COLORS, COLOR_CARD_TYPES)
-    black_cards = product(repeat('black', 4), BLACK_CARD_TYPES)
-    deck = list(chain(color_cards, black_cards))
-    shuffle(deck)
-    return deck
+    @property
+    def temp_color(self):
+        return self._temp_color
+
+    @temp_color.setter
+    def temp_color(self, color):
+        if color is not None:
+            if color not in COLORS:
+                raise ValueError('Invalid color')
+        self._temp_color = color
+
+    def place(self, other):
+        return (
+            self._color == other.color or
+            self.card_type == other.card_type or
+            other.color == 'black'
+        )
+
+
+class UnoGame:
+    def __init__(self, players):
+        if not isinstance(players, int):
+            raise ValueError('Invalid game: players must be integer')
+        if players < 2:
+            raise ValueError('Invalid game: must be at least 2 players')
+        self.deck = self._create_deck()
+        self.players = [self._create_hand() for player in range(players)]
+
+    def _create_deck(self):
+        color_cards = product(COLORS, COLOR_CARD_TYPES)
+        black_cards = product(repeat('black', 4), BLACK_CARD_TYPES)
+        all_cards = chain(color_cards, black_cards)
+        deck = [UnoCard(color, card_type) for color, card_type in all_cards]
+        shuffle(deck)
+        return deck
+
+    def _create_hand(self):
+        return [self.deck.pop() for i in range(7)]
+
+    @property
+    def current_card(self):
+        return self.deck[-1]
+
+    @property
+    def is_active(self):
+        return True
--- uno_tests.py
+++ uno_tests.py
@@ -1,6 +1,8 @@
 import pytest
 from uno import *
 
+# Test creating invalid cards throws exception
+
 with pytest.raises(TypeError):
     card = UnoCard()
 
@@ -16,14 +18,103 @@ with pytest.raises(ValueError):
 with pytest.raises(ValueError):
     card = UnoCard('purple', 1)
 
-card = UnoCard('red', 0)
-card = UnoCard('black', 'wildcard')
-
 with pytest.raises(ValueError):
     card = UnoCard('red', 'wildcard')
 
 with pytest.raises(ValueError):
     card = UnoCard('black', 1)
 
-deck = create_uno_deck()
-assert len(deck) == 108
+# Test creating valid cards
+
+card = UnoCard('red', 0)
+card = UnoCard('black', 'wildcard')
+
+# Test placing valid cards on other cards
+
+card1 = UnoCard('red', 1)
+
+card2 = UnoCard('red', 1)
+assert card1.place(card2)
+card2 = UnoCard('red', 2)
+assert card1.place(card2)
+card2 = UnoCard('red', 'skip')
+assert card1.place(card2)
+card2 = UnoCard('green', 1)
+assert card1.place(card2)
+card2 = UnoCard('green', 1)
+assert card1.place(card2)
+card2 = UnoCard('red', 1)
+assert card1.place(card2)
+card2 = UnoCard('black', 'wildcard')
+assert card1.place(card2)
+card2 = UnoCard('black', '+4')
+assert card1.place(card2)
+
+# Test placing invalid cards on other cards
+
+card1 = UnoCard('red', 1)
+
+card2 = UnoCard('green', 2)
+assert not card1.place(card2)
+card2 = UnoCard('blue', 9)
+assert not card1.place(card2)
+card2 = UnoCard('yellow', 'skip')
+assert not card1.place(card2)
+
+# Test placing valid cards on black cards
+
+card1 = UnoCard('black', 'wildcard')
+card1.temp_color = 'red'
+
+card2 = UnoCard('red', 1)
+assert card1.place(card2)
+card2 = UnoCard('red', 'skip')
+assert card1.place(card2)
+card2 = UnoCard('black', 'wildcard')
+assert card1.place(card2)
+
+# Test placing invalid cards on black cards
+
+card1 = UnoCard('black', 'wildcard')
+card1.temp_color = 'red'
+
+card2 = UnoCard('green', 1)
+assert not card1.place(card2)
+
+card2 = UnoCard('green', 'skip')
+assert not card1.place(card2)
+
+# Test creating invalid Uno Game
+
+with pytest.raises(TypeError):
+    game = UnoGame()
+
+with pytest.raises(ValueError):
+    game = UnoGame(1)
+
+with pytest.raises(ValueError):
+    game = UnoGame('foo')
+
+with pytest.raises(TypeError):
+    game = UnoGame('red', 1)
+
+card = UnoCard('red', 1)
+with pytest.raises(ValueError):
+    game = UnoGame(card)
+
+# Test creating valid Uno Game
+
+for n in range(2, 8):
+    game = UnoGame(n)
+    assert len(game.players) == n
+    for player in game.players:
+        assert len(player) == 7
+        for card in player:
+            assert isinstance(card, UnoCard)
+    assert len(game.deck) == 108 - 7*n
+
+# Test gameplay
+
+game = UnoGame(2)
+assert isinstance(game.current_card, UnoCard)
+assert game.is_active
